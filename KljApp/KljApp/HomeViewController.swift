2ce0d0004f711a48573c196b43629029247bfbeb
Bruno Stroobants <bruno.stroobants.y9084@student.hogent.be>
2018-12-05 23:22:41 +0100

    Layout Homescreen opmaken

--- KljApp/KljApp/HomeViewController.swift
+++ KljApp/KljApp/HomeViewController.swift
@@ -8,18 +8,28 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var weerImg: UIImageView!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     
+    @IBOutlet weak var weerView: UIView!
+    
     
     let weerController = WeerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Dit stelt de kleur van de LargeTitle in. Werkt niet via de attribute inspector (Bug)
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        // Hier zet men de kleur van de notificationbar naar wit, ookal zet men hier .black (Bug)
+        navigationController?.navigationBar.barStyle = .black
+        
         beschrijvingLabel.isHidden = true
         temperatuurLabel.isHidden = true
         luchtvochtigheidLabel.isHidden = true
         weerImg.isHidden = true
         spinner.isHidden = false
+        
         spinner.startAnimating()
+        
+        weerView.layer.cornerRadius = 15
         
         weerController.fetchWeerInfo { (weer) in
             if let weer = weer {
@@ -28,6 +38,7 @@ class HomeViewController: UIViewController {
         }
     }
     
+    
     func updateUI(with weer: Weer){
         DispatchQueue.main.async {
             let temp = round(weer.temperatuur)
@@ -35,7 +46,7 @@ class HomeViewController: UIViewController {
             
             self.beschrijvingLabel.text = weer.beschrijving
             self.temperatuurLabel.text = "Temperatuur: \(temp) °C"
-            self.luchtvochtigheidLabel.text = String(luchtVocht) + " %"
+            self.luchtvochtigheidLabel.text = "Luchtvochtigheid: " + String(luchtVocht) + " %"
             self.weerImg.image = UIImage(named: "\(weer.icon)")
             
             self.beschrijvingLabel.isHidden = false
@@ -43,6 +54,7 @@ class HomeViewController: UIViewController {
             self.luchtvochtigheidLabel.isHidden = false
             self.weerImg.isHidden = false
             self.spinner.isHidden = true
+            
             self.spinner.stopAnimating()
         }
     }
