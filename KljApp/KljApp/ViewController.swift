cec11756a4900f9a62d56a2a7656bb280a50abf9
Bruno Stroobants <BrunoStroobants@MacBook-Air-van-Bruno.local>
2018-12-05 10:01:43 +0100

    Init project

---
+++ KljApp/KljApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KljApp
+//
+//  Created by Bruno Stroobants on 05/12/2018.
+//  Copyright © 2018 Bruno Stroobants. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
